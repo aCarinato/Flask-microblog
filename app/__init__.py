59079c2837a7928e06ecebbf716aa7938a6531df
Alessandro Carinato <alessandro.carinato@gmail.com>
2020-07-18 14:13:04 +0200

    Added user registration and log-out functionality

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)                        # database instance
 migrate = Migrate(app, db)
 login = LoginManager(app)
+# Flask-Login needs to know what is the view function that handles logins
+login.login_view = 'login' # 'login' is the function (or endpoint) name for the login view i.e. the nam one would use in a url_for() call
 
 # the import at bottom is a workaround to circular imports
 from app import routes, models      	   # 'models' define the structure of the database
--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,18 @@
 # handlers for the application routes are written as python functions, called view functions
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
-from flask_login import current_user, login_user
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from flask import request
+from werkzeug.urls import url_parse
+from app.models import User
+from app import db
 
+# The way Flask-Login protects a view function against anonymous users is with a decorator called @login_required
+# When this is added to a view function below the @app.route the function becomes protected and will not allow access to non-authenticated users
 @app.route('/')         # creates an assocition between the URL given as an argument and the funciton
 @app.route('/index')    # when the browser requests either '/' or '/index' flask is going to invoke index()
+@login_required
 def index():
     user = {'username':'Ale'}
     posts = [
@@ -20,7 +27,7 @@ def index():
     }
     ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 # GET requests are those that return information to the client (web browser in this case)
@@ -30,7 +37,7 @@ def login():
     # a user is logged-in and navigates to the /login URL of the application: this is a mistake and needs to be avoided
     #
     if current_user.is_authenticated:   # current_user is a variable that comes with Flask-Login -> obtain the user object that represent the client of the request        return redirect(url_for('index'))
-
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():   # this does all the form processig work
         # query the database to find the user
@@ -46,7 +53,34 @@ def login():
         # if username and passwd are both correct the login_user() will register the user as logged in:
         # any future pages the user navigates to will have the current_user variable set to that user
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')        # request variable contains all the information that the client sent with the request
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
 
 
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        # creates a new user with the username, email and password provided, writes it to the database and redirects to the login propmpt
+        # so that the user can log in
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
